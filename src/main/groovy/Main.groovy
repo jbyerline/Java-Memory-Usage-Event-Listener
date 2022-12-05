3b9e3af67b69a85053ecef4be6160f5938bf003e
Jacob Byerline <jbyerline@gmail.com>
2022-12-05 02:10:59 -0800

    cleaned up code

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,24 +1,43 @@
 import java.text.DecimalFormat
+import java.time.Duration
 
 class Main {
   static void main(String[] args) {
+    double startingThreshold = 0.1d
+    // Get start time of program
+    final long startTime = System.nanoTime()
     // Percentage Formatter
     DecimalFormat df = new DecimalFormat("0.00")
     // Set initial threshold percentage
-    MemoryWarningSystem.setPercentageUsageThreshold(0.6)
+    MemoryWarningSystem.setPercentageUsageThreshold(startingThreshold)
     // Instantiate Memory Warning System
     MemoryWarningSystem mws = new MemoryWarningSystem()
     // Create a listener
     mws.addListener(new MemoryWarningSystem.Listener() {
       // Define Listener interface method
       void memoryUsageLow(long usedMemory, long maxMemory) {
+        // Calculate runtime of program so far
+        final long elapsedTime = System.nanoTime() - startTime
+        Duration duration = Duration.ofNanos(elapsedTime)
+        String formattedElapsedTime = String.format(
+                "%02d Day %02d Hour %02d Min %02d Sec elapsed.", duration.toDays(),
+                duration.toHours() % 24, duration.toMinutes() % 60, duration.toSeconds() % 60)
         // Calculate percentage of memory used
         double percentageUsed = (((double) usedMemory) / maxMemory) * 100
         // Print warning message
         System.out.println("--- WARNING: Memory usage low! ---")
         System.out.println("Memory used: " + Utils.getDynamicSpace(usedMemory) + " of " + Utils.getDynamicSpace(maxMemory) + " --> " + df.format(percentageUsed) + "%")
+        System.out.println(formattedElapsedTime)
+        if (startingThreshold < 0.9d){
+          startingThreshold = startingThreshold + 0.1d
+        } else if (startingThreshold == 0.9d){
+          startingThreshold = 0.97d
+        } else if (startingThreshold == 0.97d){
+          System.out.println("Memory Full, exiting program gracefully")
+          System.exit(1)
+        }
         // Set new threshold for 2nd alert
-        MemoryWarningSystem.setPercentageUsageThreshold(0.8)
+        MemoryWarningSystem.setPercentageUsageThreshold(startingThreshold)
       }
     })
     // Make a LinkedList of Doubles
